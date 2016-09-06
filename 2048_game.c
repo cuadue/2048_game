7f77cf035cbb78570780e6d9dc87e10ac622c4cd
Wes <wwaugh@gmail.com>
2016-09-05 18:03:21 -0700

    Add record and playback

--- 2048_game.c
+++ 2048_game.c
@@ -14,11 +14,21 @@
 #include <string.h>
 #include <time.h>
 #include <assert.h>
+#include <unistd.h>
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
 #define NROWS 4
 #define NCOLS NROWS
+
+static const char *usage =
+	"2048: A sliding tile puzzle game\n\n"
+	"Usage: %s [-r R] [-p P] [-s SEED] [-h]\n\n"
+	"\t-r\tR\tRecord to file R\n"
+	"\t-p\tP\tPlay back from file P\n"
+	"\t-s\tSEED \tUse SEED for the random number generator\n"
+	"\t-d\tDELAY\tDelay for DELAY ms when playing back\n"
+	"\t-h\t\tShow this message\n";
 
 typedef int tile;
 
@@ -26,6 +36,10 @@ struct game {
 	int turns, score;
 	tile board[NROWS][NCOLS];
 };
+
+static FILE *recfile = NULL, *playfile = NULL;
+static int batch_mode;
+static int delay_ms = 250;
 
 // place_tile() returns 0 if it did place a tile and -1 if there is no open
 // space.
@@ -215,21 +229,86 @@ int max_tile(const tile *lboard)
 	return ret;
 }
 
-int main()
+FILE *fopen_or_die(const char *path, const char *mode)
 {
-	init_curses();
+	FILE *ret = fopen(path, mode);
+	if (!ret) {
+		perror(path);
+		exit(EXIT_FAILURE);
+	}
+	return ret;
+}
 
+int get_input()
+{
+	if (playfile) {
+		char *line = NULL;
+		size_t len = 0;
+		int ret = 'q';
+		if (getline(&line, &len, playfile) > 0) {
+			ret = line[strspn(line, " \t")];
+		}
+		free(line);
+		if (!batch_mode)
+			usleep(delay_ms * 1000);
+		return ret;
+	}
+	else {
+		return getch();
+	}
+}
+
+void record(char key, const struct game *game)
+{
+	if (recfile) {
+		fprintf(recfile, "%c:%d\n", key, game->score);
+	}
+}
+
+int main(int argc, char **argv)
+{
 	const char *exit_msg = "";
-	srandom(time(NULL));
-
 	struct game game = {0};
 	int last_turn = game.turns;
+	time_t seed = time(NULL);
+	int opt;
 
+	while ((opt = getopt(argc, argv, "hr:p:s:d:")) != -1) {
+		switch (opt) {
+		case 'r':
+			recfile = fopen_or_die(optarg, "w");
+			break;
+		case 'p':
+			playfile = fopen_or_die(optarg, "r");
+			break;
+		case 's':
+			seed = atoi(optarg);
+			break;
+		case 'd':
+			delay_ms = atoi(optarg);
+			break;
+		case 'h':
+			printf(usage, argv[0]);
+			exit(EXIT_SUCCESS);
+		default:
+			fprintf(stderr, usage, argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	srandom(seed);
 	place_tile(&game);
 	place_tile(&game);
+	batch_mode = recfile && playfile;
+
+	if (!batch_mode) {
+		init_curses();
+	}
 
 	while (1) {
-		print_game(&game);
+		if (!batch_mode) {
+			print_game(&game);
+		}
 
 		if (lose_game(game)) {
 			exit_msg = "lost";
@@ -238,7 +317,8 @@ int main()
 
 		last_turn = game.turns;
 
-		switch (getch()) {
+		int key = get_input();
+		switch (key) {
 		case 'h': case KEY_LEFT: move_left(&game); break;
 		case 'j': case KEY_DOWN: move_down(&game); break;
 		case 'k': case KEY_UP: move_up(&game); break;
@@ -248,15 +328,25 @@ int main()
 			goto end;
 		}
 
-		if (last_turn != game.turns)
+		if (last_turn != game.turns) {
 			place_tile(&game);
+			record(key, &game);
+		}
 	}
 
 lose:
+	if (batch_mode) {
+		return 0;
+	}
+
 	move(7, 0);
 	printw("You lose! Press q to quit.");
 	while (getch() != 'q');
 end:
+	if (batch_mode) {
+		return 0;
+	}
+
 	endwin();
 	printf("You %s after scoring %d points in %d turns, "
 		"with largest tile %d\n",
